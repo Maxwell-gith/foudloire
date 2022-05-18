9b3960716ce7bd4a6341eab399fc8439f8904f4b
Maxime <herymaxime@gmail.com>
2022-05-18 21:09:17 +0200

    debut du slider menu

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 function burgerMenu () {
-    let navbar = document.querySelector('.navbar');
-    let burger = document.querySelector('.navbar__burger');
+    const navbar = document.querySelector('.navbar');
+    const burger = document.querySelector('.navbar__burger');
     burger.addEventListener('click', function () {
         navbar.classList.toggle('show-navbar');
     })
 }
 
 function closeMenu () {
-    let links = document.querySelector('.navbar__links');
-    let navBar = document.querySelector('.navbar');
+    const links = document.querySelector('.navbar__links');
+    const navBar = document.querySelector('.navbar');
     links.addEventListener('click', function () {
         navBar.classList.remove('show-navbar');
     })
 }
+
+const slides = document.querySelectorAll('.slide');
+const allSlides = slides.length;
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+let counter = 0;
+function nextSlide () {
+    slides[counter].classList.remove('active');
+
+    if(counter < allSlides - 1) {
+        counter++;
+    } else {
+        counter = 0;
+    }
+
+    slides[counter].classList.add('active');
+}
+
+next.addEventListener('click', nextSlide());
+
+
 burgerMenu();
 closeMenu();

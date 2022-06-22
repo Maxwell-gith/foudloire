c7df337f76a0b25b36fafab26fc7ef308046935a
Maxime <herymaxime@gmail.com>
2022-06-22 17:58:40 +0200

    menu food drinks for mobile done

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function burgerMenu () {
     })
 }
 
-function closeMenu () {
+function closeBurgerMenu () {
     const links = document.querySelector('.navbar__links');
     const navBar = document.querySelector('.navbar');
     links.addEventListener('click', function () {
@@ -14,7 +14,24 @@ function closeMenu () {
     })
 }
 
+function foodOrDrinks () {
+    const foodBtn = document.querySelector('.food__btn');
+    const drinksBtn = document.querySelector('.drinks__btn');
+    const drinksImg = document.querySelector('.drinks__img');
+    drinksBtn.addEventListener('click', function () {
+        drinksImg.classList.add('drinks__img--isOpen')
+        drinksBtn.classList.add('menu__title--isActive')
+        foodBtn.classList.remove('menu__title--isActive')
+    })
+    foodBtn.addEventListener('click', function () {
+        drinksImg.classList.remove('drinks__img--isOpen')
+        drinksBtn.classList.remove('menu__title--isActive')
+        foodBtn.classList.add('menu__title--isActive')
+    })
+}
+
 
 burgerMenu();
-closeMenu();
+closeBurgerMenu();
+foodOrDrinks();
 
